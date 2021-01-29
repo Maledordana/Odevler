55270f36c1e1bac059e42ea0a7fe3094dbc1dad4
= <=>
2021-01-29 15:29:08 +0300

    Hatalı MyDictionary

--- Gun4Odev5/MyDictionary.cs
+++ Gun4Odev5/MyDictionary.cs
@@ -4,7 +4,50 @@ using System.Text;
 
 namespace Gun4Odev5
 {
-    class MyDictionary
+    class MyDictionary<K, V> //K ve V diye isimlendirilen type ile döndürülecek
     {
+        //ana programda class new'lendiği zaman dizi oluşmalıdır bu yüzden
+        //metot dışına tanımlamış olduk
+        K[] keys; 
+        V[] values;
+
+        public MyDictionary()
+        {
+            //constructer
+            keys = new K[0];
+            values = new V[0];
+        }
+
+        public void Add(K key, V value) //K tipinde key V tipinde value gelicek
+        {
+
+            K[] kTempArray = keys;
+            //K[] tipi kTempArray isimli keys asında değişkene atandı
+            keys = new K[keys.Length + 1];
+            //keys değişkeni de sıfırlanmış oldu 1 eleman artırılıp
+            V[] vtempArray = values;
+            values = new V[values.Length + 1];
+
+            //_kTempArray = _kArray;
+            //_vTempArray = _vArray;
+
+            for (int i = 0; i < kTempArray.Length; i++)
+            {
+                //geçici uzunluğunca elemanları birbirine eşitledik
+                keys[i] = kTempArray[i];
+            }
+
+            //anlamadım
+            keys[keys.Length - 1] = key;
+
+
+            for (int i = 0; i < vtempArray.Length; i++)
+            {
+                values[i] = vtempArray[i];
+            }
+
+            values[values.Length - 1] = value;
+
+        }
     }
 }
--- Gun4Odev5/Program.cs
+++ Gun4Odev5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Gun4Odev5
 {
@@ -6,7 +7,47 @@ namespace Gun4Odev5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<int, string> Ogrenci = new Dictionary<int, string>();
+
+            Ogrenci.Add(134, "Tolga Demirer");
+            Ogrenci.Add(158, "Ümit Özkan");
+            Ogrenci.Add(115, "Kadir Aydemir");
+            Ogrenci.Add(174, "Cemal Çiftçi");
+
+            Console.Write("Öğrenci No Giriniz");
+            int No = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Console.WriteLine(Ogrenci[No]);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Öğrenci bulunamadı.");
+            }
+
+
+            MyDictionary<int, string> Talebe = new MyDictionary<int, string>();
+
+            Talebe.Add(134, "Tolga Demirer");
+            Talebe.Add(158, "Ümit Özkan");
+            Talebe.Add(115, "Kadir Aydemir");
+            Talebe.Add(174, "Cemal Çiftçi");
+
+            Console.WriteLine("Öğrenci No Giriniz ");
+            int numara = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Console.WriteLine(Talebe[numara]);
+                //hata alıyorum [] tanımlı olmadığına dair
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Öğrenci bulunamadı.");
+            }
+
+
         }
     }
 }
